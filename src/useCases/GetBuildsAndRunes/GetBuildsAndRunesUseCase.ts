a116b64a2133bcc643a8258359d022de20186502
CauaHenrique19 <cauah6002@gmail.com>
2021-10-24 12:49:02 -0300

    Adding search of initial itens

--- src/useCases/GetBuildsAndRunes/GetBuildsAndRunesUseCase.ts
+++ src/useCases/GetBuildsAndRunes/GetBuildsAndRunesUseCase.ts
@@ -5,10 +5,10 @@ import { Lanes } from '../../Enums/LanesEnum'
 
 export class GetBuilsAndRunesUseCase {
 
-    spellsArray : String[] = []
+    spellsArray: String[] = []
     aliasChampions = {}
 
-    constructor(){
+    constructor() {
         this.spellsArray = [
             'Flash',
             'Incendiar',
@@ -20,7 +20,7 @@ export class GetBuilsAndRunesUseCase {
             'Purificar',
             'Golpear'
         ]
-        
+
         this.aliasChampions = {
             'mf': 'missfortune',
             'jarvan': 'jarvaniv',
@@ -38,16 +38,26 @@ export class GetBuilsAndRunesUseCase {
         }
     }
 
-    async execute(champion: String, lane: String) : Promise<string> {
+    async execute(champion: String, lane: String): Promise<string> {
         champion = this.aliasChampions[`${champion}`] || champion
-        
+
         const resNew = await axios.get(`https://www.leagueofgraphs.com/pt/champions/builds/${champion}/${lane ? `${Lanes[`${lane}`]}` : ''}`)
         const a = cheerio.load(resNew.data)
+
+        const nameChampion = a('div.txt h2').text()
 
         const runes = a('img', 'div[style=""]')
             .toArray()
             .map(rune => a(rune).attr().alt)
             .splice(0, 6)
+
+        const initialItems = [...new Set(
+            a('img[width="48"]', 'div.championSpell')
+                .toArray()
+                .map((item, index) => index > 2 && index < 6 ? a(item).attr().alt : '')
+                .filter(item => !!item)
+            )
+        ]
 
         const items = [...new Set(
             a('img[width="48"]', 'div.championSpell')
@@ -62,12 +72,13 @@ export class GetBuilsAndRunesUseCase {
             .map(spell => a(spell).attr().alt)
             .filter(spell => this.spellsArray.includes(spell))
 
-        const title = `Runa, Build e Spells de **${champion.replace(champion.charAt(0), champion.charAt(0).toUpperCase())}**\n`
+        const title = `Runa, Build e Spells de **${nameChampion}**\n`
         const runeString = runes.map(rune => `🗡️ ${rune}\n`).join('')
+        const initialItemsString = initialItems.map(item => `⏰ ${item}\n`).join('')
         const itemsString = items.map(item => `📦 ${item}\n`).join('')
         const spellString = spells.map(spell => `🔮 ${spell}\n`).join('')
 
-        const finalString = `${title}\n${runeString}\n${itemsString}\n${spellString}`
+        const finalString = `${title}\n**⬇️ Runa: **\n${runeString}\n**⬇️ Itens iniciais: **\n${initialItemsString}\n**⬇️ Build: **\n${itemsString}\n**⬇️ Spells: **\n${spellString}`
         return finalString
     }
 }
